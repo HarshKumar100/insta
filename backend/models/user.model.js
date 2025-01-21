d23f0ab960eb7ecb83939143a262911590728840
HarshKumar100 <hk102004@gmail.com>
2025-01-22 02:21:35 +0530

    hello

---
+++ backend/models/user.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  // name: {
+  //   type: String,
+  //   required: [true, "Please enter your Name!"],
+  //   minLength: [3, "Name must contain at least 3 Characters!"],
+  //   maxLength: [30, "Name cannot exceed 30 Characters!"],
+  // },
+  // phone: {
+  //   type: Number,
+  //   required: [true, "Please enter your Phone Number!"],
+  // },
+  role: {
+    type: String,
+    required: [true, "Please select a role"],
+    enum: ["Job Seeker", "Employer"],
+  },
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  profilePicture: { type: String, default: '' },
+  bio: { type: String, default: '' },
+  gender: { type: String, enum: ['male', 'female'] },
+  followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }],
+  bookmarks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }]
+}, { timestamps: true });
+export const User = mongoose.model('User', userSchema);
